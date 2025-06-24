c3c0d34999a0cd6fe401b2e8cfe5fe9c05063ad4
Tursinbay Abdimuratov <abdimuratoovv@gmail.com>
2025-06-24 12:08:31 +0500

    Delete javascript/day09_argumentsLength.js

--- javascript/day09_argumentsLength.js
+++
@@ -1,8 +0,0 @@
-var argumentsLength = function(...args) {
-    return args.length;
-};
-
-// Test examples
-console.log(argumentsLength(1, 2, 3));     // 3
-console.log(argumentsLength());           // 0
-console.log(argumentsLength("a", null));  // 2
