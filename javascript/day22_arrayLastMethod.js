9415c837b0907affaf2f474c6335b0d2d6c6dc69
Tursinbay Abdimuratov <abdimuratoovv@gmail.com>
2025-06-24 12:20:52 +0500

    Delete javascript/day22_arrayLastMethod.js

--- javascript/day22_arrayLastMethod.js
+++
@@ -1,8 +0,0 @@
-Array.prototype.last = function() {
-    return this.length === 0 ? -1 : this[this.length - 1];
-};
-
-// Tests
-console.log([1, 2, 3].last());     // 3
-console.log([].last());            // -1
-console.log(["hello", "world"].last()); // "world"
