9f8bb26b12b77bc14620f186230f27b809b9c690
Tursinbay Abdimuratov <abdimuratoovv@gmail.com>
2025-06-24 11:44:35 +0500

    Update day01_createHelloWorld.js

--- javascript/30days/day01_createHelloWorld.js
+++ javascript/30days/day01_createHelloWorld.js
@@ -1,1 +1,16 @@
+// Day 1 - LeetCode 30 Days of JavaScript
+// Problem: Create Hello World Function
+// Link: https://leetcode.com/problems/create-hello-world-function/
 
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function() {
+        return "Hello World";
+    };
+};
+
+// Example usage:
+const f = createHelloWorld();
+console.log(f()); // Output: "Hello World"
