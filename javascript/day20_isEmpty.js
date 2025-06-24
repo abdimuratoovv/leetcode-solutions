ddd376f2b8bb57eb1f3fa02ecd961afbdc05fe3f
Tursinbay Abdimuratov <abdimuratoovv@gmail.com>
2025-06-24 12:18:57 +0500

    Delete javascript/day20_isEmpty.js

--- javascript/day20_isEmpty.js
+++
@@ -1,9 +0,0 @@
-var isEmpty = function(obj) {
-    return Object.keys(obj).length === 0;
-};
-
-// Tests
-console.log(isEmpty({}));          // true
-console.log(isEmpty([]));          // true
-console.log(isEmpty({ x: 1 }));    // false
-console.log(isEmpty([10, 20]));    // false
